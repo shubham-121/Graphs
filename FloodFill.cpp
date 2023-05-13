c160f21e83c7e01ad9331e0ec27218b36d643d05
Shubham Bhatt <81768689+shubham-121@users.noreply.github.com>
2023-05-13 12:47:24 +0530

    Create FloodFill.cpp

---
+++ FloodFill.cpp
@@ -0,0 +1,18 @@
+  private:
+    void dfs(int row, int col,  vector<vector<int>>&ans, vector<vector<int>>& image, int newColor,
+    int delRow[],int delCol[], int initial_color)
+    {
+        int n=image.size();
+        int m=image[0].size();
+        ans[row][col]=newColor;
+        for(int i=0;i<4;i++)
+        {
+            int nrow=row+delRow[i];
+            int ncol=col+delCol[i];
+            
+            if(nrow>=0&& nrow<n && ncol>=0&&ncol<m  && image[nrow][ncol]==initial_color && ans[nrow][ncol]!=newColor)
+            {
+                dfs(nrow, ncol, ans, image,newColor, delRow, delCol, initial_color);
+            }
+        }
+    }
